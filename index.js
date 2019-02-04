b89980600293a9159f4db10d1b87ce2a7e0eebaf
Mark Eaton <mark@LAdmins-MacBook-Pro.local>
2019-02-04 15:09:44 -0600

    cleanup

--- index.js
+++ index.js
@@ -4,7 +4,35 @@
  * @param {!express:Request} req HTTP request context.
  * @param {!express:Response} res HTTP response context.
  */
-exports.helloWorld = (req, res) => {
-  let message = req.query.message || req.body.message || 'Hello WorldX!';
-  res.status(200).send(message);
+
+import { BigQuery } from '@google-cloud/bigquery';
+
+async function helloWorld (req, res) {
+  
+  const tableName = process.env.tableName;
+  
+    // Creates a client
+  const bigquery = new BigQuery();
+
+  const query = `SELECT name
+    FROM \`${tableName}\`
+    WHERE state = 'TX'
+    LIMIT 100`;
+  const options = {
+    query: query,
+    // Location must match that of the dataset(s) referenced in the query.
+    location: 'US',
+  };
+
+  // Runs the query as a job
+  const [job] = await bigquery.createQueryJob(options);
+  console.log(`Job ${job.id} started.`);
+
+  // Waits for the query to finish
+  const [rows] = await job.getQueryResults();
+  
+  res.status(200).send(rows);
 };
+
+const _helloWorld = helloWorld;
+export { _helloWorld as helloWorld };
