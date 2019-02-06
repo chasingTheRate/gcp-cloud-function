d670b8c594710f044bb1828b33d894b761489b7b
Mark Eaton <mark@LAdmins-MacBook-Pro.local>
2019-02-06 12:08:25 -0600

    clean up

--- devServer.js
+++ devServer.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express')
-const helloWorld = require('./index');
+const { updateSorts } = require('./index');
 const app = express()
 const port = 3000
 
-app.get('/helloWorld', helloWorld.helloWorld);
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/updateSorts', updateSorts);
+app.get('/', (req, res) => res.send('Welcome to the Team Shelby Autobahn-Analytics Integration'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
--- index.js
+++ index.js
@@ -8,40 +8,31 @@
 const {BigQuery} = require('@google-cloud/bigquery');
 const axios = require('axios');
 
-async function helloWorld (req, res) {
+async function updateSorts (req, res) {
   
-  const tableName = process.env.tableName;
-  console.log(`tableName: ${tableName}`);
-  console.log(`Env: ${process.env.NODE_ENV}`);
-  
-    // Creates a client
-  const bigquery = new BigQuery();
+  const postUrl = process.env.postUrl;
+  const queryAsString = process.env.queryAsString
 
-  const query = `SELECT name
-    FROM \`${tableName}\`
-    WHERE state = 'TX'
-    LIMIT 100`;
+  // Creates a client
+  const bigquery = new BigQuery();
   const options = {
-    query: query,
+    query: queryAsString,
     // Location must match that of the dataset(s) referenced in the query.
     location: 'US',
   };
 
-  // Runs the query as a job
-  const [job] = await bigquery.createQueryJob(options);
-  console.log(`Job ${job.id} started.`);
-
-  // Waits for the query to finish
-  const [rows] = await job.getQueryResults();
-  
   try {
-    const response = await axios.post('https://gcp-cloud-function-receiver.herokuapp.com/helloworld', {data: rows});
+    // Runs the query as a job
+    const [job] = await bigquery.createQueryJob(options);
+    // Waits for the query to finish
+    const [rows] = await job.getQueryResults();
+    // Post results to endpoint
+    const response = await axios.post(postUrl, {data: rows});
     res.status(200).send('Sent data successfully!');
   } catch (e) {
     console.log(e);
-    res.status(400);
+    res.status(500).send('Error :(');
   }
-
 };
 
-exports.helloWorld = helloWorld;
+exports.updateSorts = updateSorts;
